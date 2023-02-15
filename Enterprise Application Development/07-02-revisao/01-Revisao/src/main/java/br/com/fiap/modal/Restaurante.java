7bc397b1334c00b166d3b7a64028087e5aa9f40a
Logon Aluno <luanssrr@hotmail.com>
2023-02-14 22:46:51 -0300

    aula de javinha revisao e exercicio -  pendente

--- Enterprise Application Development/07-02-revisao/01-Revisao/src/main/java/br/com/fiap/modal/Restaurante.java
+++ Enterprise Application Development/07-02-revisao/01-Revisao/src/main/java/br/com/fiap/modal/Restaurante.java
@@ -1,13 +1,31 @@
 package br.com.fiap.modal;
 
+import java.time.LocalDateTime;
+
 public class Restaurante {
 
 	private String nome;
 	private int capacidade;
+	private LocalDateTime dataAbertura;
+	private Culinaria culinaria;
+	
 
+	
 	public Restaurante(String nome) {
+
 		this.nome = nome;
 	}
+
+
+	public Restaurante(String nome, int capacidade, LocalDateTime dataAbertura, Culinaria culinaria) {
+//		super();
+		this.nome = nome;
+		this.capacidade = capacidade;
+		this.dataAbertura = dataAbertura;
+		this.culinaria = culinaria;
+	}
+
+
 
 	public Restaurante(String nome, int capacidade) {
 		;
@@ -35,11 +53,27 @@ public class Restaurante {
 		this.capacidade = capacidade;
 	}
 
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	
+	
+
+	public Culinaria getCulinaria() {
+		return culinaria;
+	}
+
+	public void setCulinaria(Culinaria culinaria) {
+		this.culinaria = culinaria;
+	}
+
 	@Override
 	public String toString() {
 		return "Restaurante -> Nome: " + nome.toUpperCase() + ", capacidade: " + capacidade;
 	}
-	
-	
 
 }
