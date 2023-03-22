ba7c1168033e5cc44cf2b8114e34a00d1fb5d8d4
luan.reis <luan.reis@portoseguro.com.br>
2023-03-21 21:38:58 -0300

    aula

---
+++ Hybrid Mobile App Development/aula20mar/App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+
+  const [nome, setNome] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Text>Entre com seu nome</Text>
+      <TextInput onChangeText={setNome} autoFocus={true} style={styles.textInputEmail} placeholder='Informe seu nome' />
+      <Button color="black"  onPress={_ => { nome ? alert(`Ola ${nome}`) : alert("informe seu nome")}} title={(1 == 1) ? "Entra aqui" : "Sair"} />
+      <StatusBar style="auto" />
+    </View>
+  );
+
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textInputEmail: {
+
+    width: '70%',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 40,
+    paddingHorizontal: 16,
+    margin: 10
+  },
+  button: {
+    backgroundColor: '#00aeef',
+    borderColor: 'red',
+    borderWidth: 5,
+    borderRadius: 15       
+ }
+
+
+});
