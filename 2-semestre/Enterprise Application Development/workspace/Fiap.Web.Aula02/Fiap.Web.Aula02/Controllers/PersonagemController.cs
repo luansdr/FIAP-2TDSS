81f3dd8a23ac4abbf2c32a8a2a4b27a800c8fe48
luan.reis <luan.reis@portoseguro.com.br>
2023-09-15 23:17:27 -0300

    att aula thiagera

---
+++ 2-semestre/Enterprise Application Development/workspace/Fiap.Web.Aula02/Fiap.Web.Aula02/Controllers/PersonagemController.cs
@@ -0,0 +1,42 @@
+﻿using Fiap.Web.Aula02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Aula02.Controllers
+{
+    public class PersonagemController : Controller
+    {
+        private static List<Personagem> _lista = new List<Personagem>();
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View(_lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            ViewBag.personagens = _lista;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Personagem personagem)
+        {
+            if (ModelState.IsValid)
+            {
+                personagem.Id = _lista.Count + 1;
+                _lista.Add(personagem);
+
+                ModelState.Clear();
+                ViewBag.personagens = _lista;
+                return RedirectToAction("Cadastrar");
+            }
+
+            return View();
+        }
+    }
+}
--- 2-semestre/Enterprise Application Development/workspace/Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Personagem.cs
+++ 2-semestre/Enterprise Application Development/workspace/Fiap.Web.Aula02/Fiap.Web.Aula02/Models/Personagem.cs
@@ -1,11 +1,18 @@
-﻿namespace Fiap.Web.Aula02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Web.Aula02.Models
 {
     public class Personagem
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }        
+        public string? Nome { get; set; }
+
+        [Display(Name ="Nível de energia")]
         public int NivelEnergia { get; set; }
         public bool Capa { get; set; }
+
+        [Display(Name = "Data de criação")]
+        [DataType(DataType.Date)]
         public DateTime DataCriacao { get; set; }
     }
 }
